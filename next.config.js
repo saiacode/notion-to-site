84357839a4c2746025d8716727b0b7455a823737
Alexander Ditzend <alex@saia.ar>
2022-09-28 11:07:44 -0300

    works

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
+  images: {
+    domains: [
+      's3.us-west-1.amazonaws.com',
+      's3.us-west-2.amazonaws.com',
+      's3.us-west-3.amazonaws.com',
+      's3.us-east-2.amazonaws.com',
+      's3.us-east-1.amazonaws.com',
+      's3.us-east-3.amazonaws.com',
+      's3.us-south-1.amazonaws.com',
+      's3.us-south-2.amazonaws.com',
+      's3.us-south-3.amazonaws.com',
+      's3.us-north-1.amazonaws.com',
+      's3.us-north-2.amazonaws.com',
+      's3.us-north-3.amazonaws.com',
+      's3.us-central-1.amazonaws.com',
+      's3.us-central-2.amazonaws.com',
+      's3.us-central-3.amazonaws.com',
+    ],
+  },
 }
--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,26 @@
 import Head from 'next/head'
-// import Hero from '../components/hero'
+import Hero from '../components/hero'
 import styles from '../styles/Home.module.css'
+import * as page from '../lib/page'
 
-console.log('My Application Version', process.env.PAGE_ID);
+export const getStaticProps = async() => {
+  const headLine = await page.getHeadline(`${process.env.PAGE_ID}`);
+  const children = await page.getChildren(`${process.env.PAGE_ID}`);
+  const coverUrl = await page.getCoverUrl(`${process.env.PAGE_ID}`);
+  const iconUrl = await page.getIconUrl(`${process.env.PAGE_ID}`);
+  return {
+    props: {
+      headLine,
+      children,
+      coverUrl,
+      iconUrl,
+    },
+  };
+}
 
-export default function Home() {
+
+export default function Home({ ...props }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-       
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <Hero {...props} />
   )
 }
